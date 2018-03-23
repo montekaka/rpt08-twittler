9a42d9587e6a07014729e93c972b7380718a5fa7
montekaka <josh.chen.jia@gmail.com>
2018-03-22 23:20:43 -0700

    add setTabName function and filter tweets on selected user

--- script.js
+++ script.js
@@ -4,7 +4,13 @@ var $body;
 
 $(document).ready(function(){		
 	$body = $('body');	
-	tweets = streams.home;
+	var main_tweets = streams.home;
+	var tweets = main_tweets;
+	var selectedHome = true;
+	var selectedTab = "Home";
+
+	setTabName(selectedTab);
+
 	loadTweets(tweets, $body, tweetId, function(x) {
 		tweetId = x;
 	});	
@@ -13,18 +19,43 @@ $(document).ready(function(){
 		refresh();
 	});		
 
-	setInterval(function(){
-		refresh();
-	}, 3000);
+
+	$('div.user').click(function(){
+		selectedHome = false;		
+		var user = $(this).text();
+		selectedTab = user;
+		setTabName(selectedTab);		
+		var tweets = [];
+		for(var i = 0; i < main_tweets.length; i++) {
+			if("@"+main_tweets[i].user === user) {
+				tweets.push(main_tweets[i]);
+			}
+		}
+		tweetId = 0;
+	})
+
+	// setInterval(function(){
+	// 	refresh();
+	// }, 3000);
 });
 
 var loadTweets = function(tweets, $body, id, cb ){
 
 	for(var i = id; i < tweets.length; i++) {
+		var $div = $('<div class="tweet"></div>');
+
 		var tweet = tweets[i];
-	  var $tweet = $('<div></div>');
-	  $tweet.text('@' + tweet.user + ': ' + tweet.message + ' - ' +tweet.created_at);
-	  $tweet.appendTo($body);	      
+	  var $tweet = $('<div class="message"></div>');
+	  $tweet.text(tweet.message);
+	  var $tweet_created_at = $('<div class="created_at"></div>');
+	  $tweet_created_at.text(tweet.created_at);	  
+	  var $user = $('<div class="user"></div>');
+	  $user.text('@' + tweet.user);
+
+	  $user.appendTo($div);	  
+		$tweet_created_at.appendTo($div);	  
+	  $tweet.appendTo($div);	      	  
+	  $div.appendTo($body);    
 	  id += 1;				  
 	}
 	cb(id);
@@ -34,4 +65,8 @@ var refresh = function() {
 	loadTweets(tweets, $body, tweetId, function(x) {
 		tweetId = x;
 	});		
+}
+
+var setTabName = function(tabname) {
+	$('div.tabname').text(tabname);
 }
